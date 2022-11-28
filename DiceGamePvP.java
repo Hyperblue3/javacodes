3bc3ee08a397def74eb722bb4da0b35ffbbd4e81
Hyperblue3 <118739056+Hyperblue3@users.noreply.github.com>
2022-11-28 19:39:21 +0300

    Update and rename cevap5.java to DiceGamePvP.java

--- DiceGamePvP.java
+++ DiceGamePvP.java
@@ -1,6 +1,6 @@
 package ödev2;
 
-public class cevap5 {
+public class DiceGamePvP {
 	public static void main(String[] args ) {
 		
 	    int zar1;
@@ -21,11 +21,11 @@ public class cevap5 {
 			 zar4=(int) (Math.random()*6+1);
 			 int zartoplam1=zar1+zar2;
 				int zartoplam2=zar3+zar4;//toplamlar
-			System.out.println("Oyunda "+i+". tur başlıyor");
-			System.out.println("1. turda 1. oyuncunun zar toplamlari: "+(zar1+zar2));
+			System.out.println("In game "+i+". turn starting");
+			System.out.println("on 1st turn sum of the 1st player's dices: "+(zar1+zar2));
 			System.out.println("1. zar değeri = "+zar1+ " ve 2. zar değeri = "+zar2+ " toplami = "+(zar1+zar2));
-			System.out.println("1. turda 2. oyuncunun zar toplamlari: "+(zar3+zar4));
-			System.out.println("1. zar değeri = "+zar3+ " ve 2. zar değeri = "+zar4 +" toplami = "+(zar3+zar4));
+			System.out.println("on 1st turn sum of the 2nd player's dices: "+(zar3+zar4));
+			System.out.println("value of 1st dice = "+zar3+ " value of 2nd dice = "+zar4 +" sum of them = "+(zar3+zar4));
 				if (zartoplam1>zartoplam2) {
 					System.out.println("Zarlar arasindaki fark= "+(zartoplam1-zartoplam2));
 					System.out.println("Birinci oyuncu "+(zartoplam1-zartoplam2)+"para kazandi");
@@ -34,24 +34,24 @@ public class cevap5 {
 					System.out.println("Birinci oyuncunun parası= "+money1+"İkinci oyuncunun parası "+money2);
 		              }
 					if (zartoplam2>zartoplam1) {
-						System.out.println("Zarlar arasindaki fark= "+(zartoplam2-zartoplam1));
-						System.out.println("İkinci oyuncu "+(zartoplam2-zartoplam1)+"para kazandi");
+						System.out.println("Diffirence of dices= "+(zartoplam2-zartoplam1));
+						System.out.println("2nd player won "+(zartoplam2-zartoplam1)+" $");
 						money2 +=(zartoplam2-zartoplam1);
 						money1 -=(zartoplam2-zartoplam1);
-						System.out.println("Birinci oyuncunun parası= "+money1+"İkinci oyuncunun parası "+money2);
+						System.out.println("Money of the 1st player= "+money1+"Money of the 2nd player "+money2);
 						
 					}
 					if(zartoplam1==zartoplam2) {
-						System.out.println("Zarlar arasindaki fark= "+(zartoplam2-zartoplam1));
+						System.out.println("Diffirences between dices= "+(zartoplam2-zartoplam1));
 					}
 		}
 		
 		while(money1 >0 && money2 >0);// oyunculardan birisinin parası bitince kadar devam etme kosulu
 		if(money1 >0) {
-			System.out.println("1. oyuncu "+i+". turda "+money1+" para ile kazandı");//kazanan oyuncuyu belirleme
+			System.out.println("1st player on "+i+".turn won with "+money1+"$");//kazanan oyuncuyu belirleme
 		}
 		if(money2 >0) {
-			System.out.println("2. oyuncu "+i+". turda "+money2+" para ile kazandı");
+			System.out.println("2nd player on "+i+". turn won with "+money2+"$");
 		}
 		
 
